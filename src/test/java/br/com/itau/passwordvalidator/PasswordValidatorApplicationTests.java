4ec0cd8b69f073d494be0a4c2e15e0f16ccc6c18
murilo.felpeto <murilo.felpeto@gmail.com>
2020-11-28 15:52:39 -0300

    Innitial commit

---
+++ src/test/java/br/com/itau/passwordvalidator/PasswordValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.passwordvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
